3a9008be70f222afa3f4dc29a2a1298726e8ca0c
sahiru <sahiruwrcg@gmail.com>
2025-03-09 21:19:13 +0530

    remove email handling from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
 'use client'
 import FeedbackForm from "@/components/feedback-form"
-import { useEffect, useState } from "react"
 
 export default function Home() {
-  const [email, setEmail] = useState<string | null>(null);
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const encodedEmail = urlParams.get("email");
-    if (encodedEmail) {
-      const decodedEmail = atob(encodedEmail);
-      setEmail(decodedEmail);
-    }
-  }, []);
-
   return (
     <main className="min-h-screen bg-white w-full">
-      <FeedbackForm email={email} />
+      <FeedbackForm />
     </main>
   )
 }
